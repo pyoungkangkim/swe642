2ebb59b3e0e14c4b095292bf05d704713cb7fee1
mlslayer <mlslayergithub@gmail.com>
2022-04-17 00:08:58 -0400

    x

--- swe645/hw3/angular-survey/src/app/app-routing.module.ts
+++ swe645/hw3/angular-survey/src/app/app-routing.module.ts
@@ -1,8 +1,11 @@
 import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
 import {AllSurveysComponent} from "./all-surveys/all-surveys.component";
+import {HomeComponent} from "./home/home.component";
 
-const routes: Routes = [{path: 'allSurveys', component: AllSurveysComponent}]
+const routes: Routes = [
+  {path: '', component: HomeComponent},
+  {path: 'allSurveys', component: AllSurveysComponent}]
 // const routes: Routes = [{path: '/all-surveys', component: AllSurveysComponent},
 //   {path: 'products/:productId', component: ProductDetailsComponent},];
 
