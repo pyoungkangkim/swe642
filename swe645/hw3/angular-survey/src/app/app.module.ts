ef00d419f8362ec2d16fa65d6608e9c823165e87
mlslayer <mlslayergithub@gmail.com>
2022-04-17 00:01:55 -0400

    x

--- swe645/hw3/angular-survey/src/app/app.module.ts
+++ swe645/hw3/angular-survey/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AllSurveysComponent } from './all-surveys/all-surveys.component';
+import { SurveyComponent } from './survey/survey.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    AllSurveysComponent
+    AllSurveysComponent,
+    SurveyComponent
   ],
   imports: [
     BrowserModule,
