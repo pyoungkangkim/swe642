35ddfc46e242b51221678e35d0afc43bbc184391
mlslayer <mlslayergithub@gmail.com>
2022-04-16 23:57:42 -0400

    x

--- swe645/hw3/angular-survey/src/app/app-routing.module.ts
+++ swe645/hw3/angular-survey/src/app/app-routing.module.ts
@@ -1,10 +1,17 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
+import {AllSurveysComponent} from "./all-surveys/all-surveys.component";
 
-const routes: Routes = [];
+const routes: Routes = [{path: 'allSurveys', component: AllSurveysComponent}]
+// const routes: Routes = [{path: '/all-surveys', component: AllSurveysComponent},
+//   {path: 'products/:productId', component: ProductDetailsComponent},];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes),
+
+
+  ],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
--- swe645/hw3/angular-survey/src/app/app.module.ts
+++ swe645/hw3/angular-survey/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AllSurveysComponent } from './all-surveys/all-surveys.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AllSurveysComponent
   ],
   imports: [
     BrowserModule,
